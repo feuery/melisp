c87f9826a0eb7072708284379a80e9b783289ee3
Feuer <feuer@oiliunoir>
2014-12-17 21:49:57 +0200

    Eval() calls are certainly doing something stupid. Let's change them to return std::string.

--- src/c/main.cpp
+++ src/c/main.cpp
@@ -179,6 +179,8 @@ string to_string(vector<node*>& vec)
   for(unsigned int i=0; i<vec.size(); i++)
     {
       LEAVE_TYPE type = vec.at(i)->type();
+      buffer += leave_to_string(type);
+      buffer += " | ";
       buffer += vec.at(i)->eval();
       buffer += "\n";
     }
